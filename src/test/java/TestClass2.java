af72ece8b3654aaaec4ecaf839a95e6efeaa8f43
slomakov <stas.lomakov@gmail.com>
2021-09-10 22:05:26 +0300

    tests

--- src/test/java/TestClass2.java
+++ src/test/java/TestClass2.java
@@ -1,20 +1,20 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestClass2 {
 
     @Test
     public void testMethod1() {
-        System.out.println("+++++++++++++++++++++++++++++++++++");
+        Assert.assertTrue(true);
     }
 
     @Test
     public void testMethod2() {
-        String client_id = SystemPropertiesReader.getProperty("client_id");
-        if (client_id.equals("qwerty")) System.out.println("QWERTY!!!");
+        Assert.assertTrue(false);
     }
 
     @Test
     public void testMethod3() {
-        System.out.println("+++++++++++++++++++++++++++++++++++");
+        Assert.assertTrue(true);
     }
 }
